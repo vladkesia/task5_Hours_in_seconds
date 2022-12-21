7d0d49ce7839aa059cc00cb9ece06dc46d462f1a
Kesya <kesyavlad74@gmail.com>
2022-12-22 00:21:17 +0200

    cheng let

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const hours = +prompt('напишіть скільки годин')
 
 if (!isNaN(hours)&& hours>0){
-    let result = hours * 3600;
+    const result = hours * 3600;
     alert(`${result} секунд`)
 }else {
     alert('Треба ввести число яке буде більше 0')
