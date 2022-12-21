c73306cbb3f2156124bf24e3c827063aa28cf168
Kesya <kesyavlad74@gmail.com>
2022-12-21 21:22:34 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let hours = +prompt('напишіть скільки годин')
+
+if (!isNaN(hours)&& hours>0){
+    let result = hours * 3600;
+    alert(`${result} секунд`)
+}else {
+    alert('Треба ввести число яке буде більше 0')
+}
